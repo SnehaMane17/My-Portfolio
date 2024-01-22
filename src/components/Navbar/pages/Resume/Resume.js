8b6e92e431fdf66cf4354c465bd0ad7a8db49d78
SnehaMane17 <snehamane171998@gmail.com>
2024-01-22 10:55:05 +0530

    fix: resume added

--- src/components/Navbar/pages/Resume/Resume.js
+++ src/components/Navbar/pages/Resume/Resume.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { Document, Page, pdfjs } from "react-pdf";
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from '../../../../assests/SnehaMaratheCV.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 export const Resume = () => {
+
+    const width = window.innerWidth;
+
     return (
         <div>
-            resume
-        </div>
+        <Container className="resume-section" style={{ padding: "20px 0" }}>
+       
+          <Row style={{ justifyContent: "center", position: "relative" }}>
+            <Button
+              variant="primary"
+              href={pdf}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV
+            </Button>
+          </Row>
+  
+          <Row className="resume" style={{marginTop: '20px'}}>
+            <Document file={pdf} className="d-flex justify-content-center">
+              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            </Document>
+          </Row>
+  
+         
+        </Container>
+      </div>
     )
 }
